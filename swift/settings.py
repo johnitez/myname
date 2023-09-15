baec8e72821635fbe1444b1513a1b173066447e7
johnitez <145155690+johnitez@users.noreply.github.com>
2023-09-15 22:58:49 +0300

    Update settings.py

--- swift/settings.py
+++ swift/settings.py
@@ -1,5 +1,8 @@
-from pathlib import Path
 import os
+from pathlib import Path
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,9 +30,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'developers',
+    'django_heroku'
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,7 +136,7 @@ EMAIL_USE_TLS = True
 
 
 # Activate Django-Heroku.
-
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
